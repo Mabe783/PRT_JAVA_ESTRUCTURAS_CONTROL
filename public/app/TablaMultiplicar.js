14ed34e32b2ed5fec17641c0783771d25bcbcd58
Mabe783 <mbacosta3@espe.edu.ec>
2025-05-20 21:22:07 -0500

    mejora en el codigo de tabla de multiplicar

--- public/app/TablaMultiplicar.js
+++ public/app/TablaMultiplicar.js
@@ -1,4 +1,5 @@
-let numero = parseInt(prompt("Ingresa un número para ver su tabla de multiplicar (del 1 al 12):"));
+let entrada = prompt("Ingresa un número para ver su tabla de multiplicar (del 1 al 12):");
+let numero = parseInt(entrada);
 
 for (let i = 1; i <= 12; i++) {
     console.log(`${numero} x ${i} = ${numero * i}`);
